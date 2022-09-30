364fc79b227e9950ca3d7849847c8580a01d2a3f
Eduardo Cardoso Fernandes Neto <ex_mate@hotmail.com>
2022-09-29 22:44:04 -0300

    Add project files.

---
+++ ReclameAquiMunicipio/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ReclameAquiMunicipio;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ReclameAquiMunicipio/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ReclameAquiMunicipio;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
